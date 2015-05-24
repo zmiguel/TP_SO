a012f4010563e6b7447398a88d5c9ac48a7d474b
ZMiguel Alves <zmiguel.alves@gmail.com>
2015-05-24 20:18:24 +0100

    edit servidor
    falta acabar os dados do labirinto!!!

--- servidor.c
+++ servidor.c
@@ -40,8 +40,9 @@ int valida_jogador(){
 }
 
 //MELHORAR ISTO - ARRANJAR MELHOR MANEIRA DE DEFINIR VALORES NAS ESTRUTURAS E DE FORMA ALEATÓRIA USSANDO A func obtem_rand(min,max)!
+
 //----------DECLARAÇÃO-DE-MONSTROS-----------
-/*monst morcego = {"Morcego", obtem_rand(4,5), obtem_rand(3,4), obtem_rand(4,5), obtem_rand(0,1)};
+monst morcego = {"Morcego", obtem_rand(4,5), obtem_rand(3,4), obtem_rand(4,5), obtem_rand(0,1)};
 morcego.ataque = obtem_rand(4,5);
 
 monst escorpiao = {"Escorpião", 7, 5, 3};
@@ -49,15 +50,15 @@ monst lobisomem = {"Lobisomem", 8, 6, 6};
 monst urso = {"Urso", 10, 10, 8};
 monst boss = {"Boss", obtem_rand(10,12), 15, 15};
 //------------DECLARACAO-DE-OBJECTOS---------
-objecto sandes = {"Sandes", 0.5, 10, 0 , 0, 1};
-objecto aspirina = {"Aspirina", 0.1, 20, 0, 0, 1};
-objecto xarope = {"Xarope", 1, 4, 0, 0, 1};
-objecto faca = {"Faca", 2, 5, 5, 0, 0};
-objecto espada = {"Espada", 8, 3, 8, 2, 0};
-objecto granado = {"Granada", 1, 2, 30, 0, 1};
-objecto escudo = {"Escudo", 4, 4, 5, 0, 0};
-objecto moeda = {"Moeda", 0.1, 5, 0, 0};
-*/
+objecto sandes =    {"Sandes",  0.5, 10, 4, 0 , 0, 1}; //id 1
+objecto aspirina =  {"Aspirina",0.1, 20, 6, 0, 0, 1}; //id 2
+objecto xarope =    {"Xarope",  1, 4, 2, 0, 0, 1}; //id 3
+objecto faca =      {"Faca",    2, 5, 0, 5, 0, 0}; //id 4
+objecto espada =    {"Espada",  8, 3, 0, 8, 2, 0}; //id 5
+objecto granado =   {"Granada", 1, 2, 0, 30, 0, 1}; //id 6
+objecto escudo =    {"Escudo",  4, 4, 0, 5, 0, 0}; //id 7
+objecto moeda =     {"Moeda",   0.1, 5, 0, 0, 0}; //id 8
+
 
 int fd_servidor, fd_cliente;
 
@@ -66,7 +67,137 @@ int main(void){
 
     MENSAGEM msg;
 
-    //MATRIZ LABIRINTO, DECLARAÇÃO ETC PRA DEPOIS IMPLEMENTAR A BAIXO...
+    /* CRIAÇAO DAS SALAS E CONTEUDO DAS SALAS DO JOGO */
+
+        //dados das salas
+        //linha 1
+        lab[0][0].prob_monst = 10;
+        lab[0][0].type = 1;
+        lab[0][0].x = 1;
+        lab[0][0].y = 2;
+        lab[0][0].pass_n = 0;
+        lab[0][0].pass_s = 1;
+        lab[0][0].pass_e = 0;
+        lab[0][0].pass_o = 0;
+
+        lab[0][1].prob_monst = 20;
+        lab[0][1].type = 0;
+        lab[0][1].x = 1;
+        lab[0][1].y = 2;
+        lab[0][0].pass_n = 0;
+        lab[0][0].pass_s = 1;
+        lab[0][0].pass_e = 1;
+        lab[0][0].pass_o = 0;
+
+        lab[0][2].prob_monst = 40;
+        lab[0][2].type = 0;
+        lab[0][2].x = 1;
+        lab[0][2].y = 3;
+        lab[0][0].pass_n = 0;
+        lab[0][0].pass_s = 0;
+        lab[0][0].pass_e = 0;
+        lab[0][0].pass_o = 1;
+
+        lab[0][3].prob_monst = 66;
+        lab[0][3].type = 0;
+        lab[0][3].x = 1;
+        lab[0][3].y = 4;
+        lab[0][0].pass_n = 0;
+        lab[0][0].pass_s = 1;
+        lab[0][0].pass_e = 0;
+        lab[0][0].pass_o = 0;
+
+        lab[0][4].prob_monst = 100;
+        lab[0][4].type = 5;
+        lab[0][4].x = 1;
+        lab[0][4].y = 5;
+        lab[0][0].pass_n = 0;
+        lab[0][0].pass_s = 1;
+        lab[0][0].pass_e = 0;
+        lab[0][0].pass_o = 0;
+
+        //linha 2
+        lab[1][0].prob_monst = 10;
+        lab[1][0].type = 0;
+        lab[1][0].x = 2;
+        lab[1][0].y = 2;
+
+        lab[1][1].prob_monst = 20;
+        lab[1][1].type = 0;
+        lab[1][1].x = 2;
+        lab[1][1].y = 2;
+
+        lab[1][2].prob_monst = 40;
+        lab[1][2].type = 0;
+        lab[1][2].x = 2;
+        lab[1][2].y = 3;
+
+        lab[1][3].prob_monst = 66;
+        lab[1][3].type = 0;
+        lab[1][3].x = 2;
+        lab[1][3].y = 4;
+
+        lab[1][4].prob_monst = 80;
+        lab[1][4].type = 0;
+        lab[1][4].x = 2;
+        lab[1][4].y = 5;
+
+        //linha 3
+        lab[2][0].prob_monst = 10;
+        lab[2][0].type = 0;
+        lab[2][0].x = 3;
+        lab[2][0].y = 2;
+
+        lab[2][1].prob_monst = 20;
+        lab[2][1].type = 0;
+        lab[2][1].x = 3;
+        lab[2][1].y = 2;
+
+        lab[2][2].prob_monst = 0;
+        lab[2][2].type = 3;
+        lab[2][2].x = 3;
+        lab[2][2].y = 3;
+
+        lab[2][3].prob_monst = 66;
+        lab[2][3].type = 0;
+        lab[2][3].x = 3;
+        lab[2][3].y = 4;
+
+        lab[2][4].prob_monst = 0;
+        lab[2][4].type = 4;
+        lab[2][4].x = 3;
+        lab[2][4].y = 5;
+
+        //linha 4
+        lab[3][0].prob_monst = 10;
+        lab[3][0].type = 0;
+        lab[3][0].x = 4;
+        lab[3][0].y = 2;
+
+        lab[3][1].prob_monst = 20;
+        lab[3][1].type = 0;
+        lab[3][1].x = 4;
+        lab[3][1].y = 2;
+
+        lab[3][2].prob_monst = 40;
+        lab[3][2].type = 0;
+        lab[3][2].x = 4;
+        lab[3][2].y = 3;
+
+        lab[3][3].prob_monst = 66;
+        lab[3][3].type = 0;
+        lab[3][3].x = 4;
+        lab[3][3].y = 4;
+
+        lab[3][4].prob_monst = 80;
+        lab[3][4].type = 0;
+        lab[3][4].x = 4;
+        lab[3][4].y = 5;
+
+        //textos das salas
+
+
+    /* FIM DA CRIAÇÃO DAS SALAS */
 
     /* VERIFICAR SE EXISTE "CP" DO SERVIDOR (access) -- APENAS UM!!!*/
     if(access("CPservidor", F_OK)==0){
@@ -90,23 +221,23 @@ int main(void){
             continue;
         }
         else if(strcmp(msg.op1,"mov")==0){
-            if(strcmp(msg.op2,"norte")==0){
-                printf("Recebi um pedido para mover o jogador para o norte...\n");
+            if(strcmp(msg.op2,"frente")==0){
+                printf("Recebi um pedido para mover o jogador para a frente...\n");
                 printf("Falta implementar!\n");
                 /*LÓGICA DE JOGO NORTE*/
             }
-            else if(strcmp(msg.op2,"sul")==0){
-                printf("Recebi um pedido para mover o jogador para o sul...\n");
+            else if(strcmp(msg.op2,"tras")==0){
+                printf("Recebi um pedido para mover o jogador para tras...\n");
                 printf("Falta implementar!\n");
                 /*LÓGICA DE JOGO SUL*/
             }
-            else if(strcmp(msg.op2,"este")==0){
-                printf("Recebi um pedido para mover o jogador para o este...\n");
+            else if(strcmp(msg.op2,"direita")==0){
+                printf("Recebi um pedido para mover o jogador para a direita...\n");
                 printf("Falta implementar!\n");
                 /*LÓGICA DE JOGO ESTE*/
             }
-            else if(strcmp(msg.op2,"oeste")==0){
-                printf("Recebi um pedido para mover o jogador para o este...\n");
+            else if(strcmp(msg.op2,"esquerda")==0){
+                printf("Recebi um pedido para mover o jogador para a esquerda...\n");
                 printf("Falta implementar!\n");
                 /*LÓGICA DE JOGO OESTE*/
             }
@@ -118,6 +249,7 @@ int main(void){
         else if(strcmp(msg.op1,"ver")==0){
             //FUNCOES PARA VISUALIZAR O QUE SE PASSA AO REDOR
             printf("Falta implementar!\n");
+
         }
         /* ABRIR "CP" DO CLIENTE (open - O_WRONLY) */
         fd_cliente = open(msg.endereco, O_WRONLY);
--- util.h
+++ util.h
@@ -6,21 +6,40 @@ typedef struct entidade_utilizador{
 
 typedef struct mensagem{
 	char op1[TAM], op2[TAM];
+	char resposta[150];
     char endereco[20];
     int desliga;
 }MENSAGEM;
 
 typedef struct jogador1{
     int vida;
+    char direccao;
 }jog_1;
 
 jog_1 jogador1 = {20};
 //p_princ jogador2 = {20};
+
+typedef struct {
+    char entrada[100]; //texto
+    char monst[100]; //texto
+    int x, y; //coordenadas da sala
+    int prob_monst; //% de probabilidade de aparecer um monstro
+    int type; // 0 = normal, 1 = p1 start, 2 = p2 start, 3 = meeting room, 4 = loot room, 5 = boss room.
+    int pass_n; // 0 = nao, 1 = sim
+    int pass_s; // 0 = nao, 1 = sim
+    int pass_e; // 0 = nao, 1 = sim
+    int pass_o; // 0 = nao, 1 = sim
+}LAB;
+
+LAB lab[5][5];
+
+
 //-------------ESTRUTURA-DE-OBJECTOS---------
 typedef struct objectos{
     char nome[20];
     int peso;
     int raridade;
+    int vida;
     int forca_ataque;
     int forca_defesa;
     int max_usos;
@@ -34,31 +53,3 @@ typedef struct monstros{
     int agress_pass;  //0 - Passivo, 1 - Agressivo
 }monst;
 
-//-------------------SALAS---------------------------- ISTRO TEM QUE SER REVISTI - AS SALAS
-typedef struct sala{
-    char descricao[100];
-    char descricao_monst[100];
-    int nivel;
-    int prob_monstro;
-}sala;
-//----------------------------------------------------------------------
-typedef struct{
-	char op;
-	int num1,num2;
-	float res;
-	char endereco[20];
-}MSG;
-
-typedef struct{
-    char comando[15];
-    char opt[25];
-    char resp[250];
-    char respopt[25];
-    char extra[50];
-    char endereco[20];
-}COMMS;
-
-typedef struct mensagem{
-	char op1, op2;
-    char endereco[20];
-}DATA;
