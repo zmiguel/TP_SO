3af86755d3e615da70fce03e4312bb0937f5c5f6
andrebento18 <andrebento1995@sapo.pt>
2015-05-24 01:13:15 +0100

    Update util.h

--- util.h
+++ util.h
@@ -1,8 +1,20 @@
-typedef struct jogador{
-    int vida;
-}j_princ;
+#define TAM 10
+typedef struct entidade_utilizador{
+    char nome[TAM], endereco[20];
+    int pass;
+}EU;
 
-j_princ jogador1 = {20};
+typedef struct mensagem{
+	char op1[TAM], op2[TAM];
+    char endereco[20];
+    int desliga;
+}MENSAGEM;
+
+typedef struct jogador1{
+    int vida;
+}jog_1;
+
+jog_1 jogador1 = {20};
 //p_princ jogador2 = {20};
 //-------------ESTRUTURA-DE-OBJECTOS---------
 typedef struct objectos{
@@ -13,37 +25,23 @@ typedef struct objectos{
     int forca_defesa;
     int max_usos;
 }objecto;
-//------------DECLARACAO-DE-OBJECTOS---------
-objecto sandes = {"Sandes", 0.5, 10, 0 , 0, 1};
-objecto aspirina = {"Aspirina", 0.1, 20, 0, 0, 1};
-objecto xarope = {"Xarope", 1, 4, 0, 0, 1};
-objecto faca = {"Faca", 2, 5, 5, 0, 0};
-objecto espada = {"Espada", 8, 3, 8, 2, 0};
-objecto granado = {"Granada", 1, 2, 30, 0, 1};
-objecto escudo = {"Escudo", 4, 4, 5, 0, 0};
-objecto moeda = {"Moeda", 0.1, 5, 0, 0};
-
 //----------ESTRUTURA-DE-MONSTROS------------
 typedef struct monstros{
     char nome[20];
-    int vida;
-    int defesa;
     int ataque;
+    int defesa;
+    int vida;
+    int agress_pass;  //0 - Passivo, 1 - Agressivo
 }monst;
-//----------DECLARAÇÃO-DE-MONSTROS-----------
-monst morcego = {"Morcego", 4, 3, 1};
-monst escorpiao = {"Escorpião", 7, 5, 3};
-monst lobisomem = {"Lobisomem", 8, 6, 6};
-monst urso = {"Urso", 10, 10, 8};
-monst boss = {"Boss", 15, 15, 12};
-//----------SALAS----------------------------
+
+//-------------------SALAS---------------------------- ISTRO TEM QUE SER REVISTI - AS SALAS
 typedef struct sala{
     char descricao[100];
     char descricao_monst[100];
     int nivel;
     int prob_monstro;
 }sala;
-
+//----------------------------------------------------------------------
 typedef struct{
 	char op;
 	int num1,num2;
